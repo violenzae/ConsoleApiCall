4f7ab54748fbbc1ff15197d49d518ab8f1a03eec
Rachel Schieferstein <violenza@gmail.com>
2020-03-30 13:45:57 -0700

    adding initial files, editing program.cs to include apicall method
    Co-authored-by: Patrick Delaney <prestwick97@gmail.com>
    Co-authored-by: Jose Amesquita <joseamesquita11@gmail.com>

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace ConsoleApiCall
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var apiCallTask = ApiHelper.ApiCall("[YOUR-API-KEY-HERE]");
+      var result = apiCallTask.Result;
+      Console.WriteLine(result);
+    }
+  }
+  class ApiHelper
+  {
+    public static async Task<string> ApiCall(string apiKey)
+    {
+      RestClient client = new RestClient("https://api.nytimes.com/svc/topstories/v2");
+      RestRequest request = new RestRequest($"home.json?api-key={apiKey}", Method.GET);
+      var response = await client.ExecuteTaskAsync(request);
+      return response.Content;
+    }
+  }
+}
